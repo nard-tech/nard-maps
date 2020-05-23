ddd648e6d647f63288903c0c9508fb2a630fadbe
Shu Fujita <s.fujita@nard.tech>
2020-05-24 01:23:28 +0900

    Add actions to WaterSupplyPointsController and routing settings

--- app/controllers/water_supply_points_controller.rb
+++ app/controllers/water_supply_points_controller.rb
@@ -1,2 +1,18 @@
 class WaterSupplyPointsController < ApplicationController
+  before_action :set_water_supply_point, only: :show
+
+  def index
+    @water_supply_points = WaterSupplyPoint.with_coordinates
+    render json: @water_supply_points.map { |object| WaterSupplyPointSerializer.new(object).serializable_hash }
+  end
+
+  def show
+    render json: WaterSupplyPointSerializer.new(@water_supply_point).serializable_hash
+  end
+
+  private
+
+  def set_water_supply_point
+    @water_supply_point = WaterSupplyPoint.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :water_supply_points, only: %i[index show]
 end
