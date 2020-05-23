9a256820862829c8d908c9cc901e868a21785757
Shu Fujita <s.fujita@nard.tech>
2020-05-24 01:22:08 +0900

    Add scope WaterSupplyPoint.with_coordinates

--- app/models/water_supply_point.rb
+++ app/models/water_supply_point.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class WaterSupplyPoint < ApplicationRecord
   enum status: { '開設' => 1, '閉鎖' => 2, '不明' => 3 }
+
+  scope :with_coordinates, -> { where.not(latitude: nil).where.not(longitude: nil) }
 end
