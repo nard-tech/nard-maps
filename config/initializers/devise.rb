c8171a4aaa1149c84866dcc44eccf0a811c8ebf2
Shu Fujita <s.fujita@nard.tech>
2020-05-23 22:32:37 +0900

    Fix config of mailer_sender in config/initializers/devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "\"#{Settings.title}\" <admin@nard-maps-covid19.herokuapp.com>"
+  config.mailer_sender = "\"#{Settings.site_title}\" <admin@nard-maps-covid19.herokuapp.com>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
