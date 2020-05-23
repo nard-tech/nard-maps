46bab29c81fe2404183b6a97ff24b7f473b2132f
Shu Fujita <s.fujita@nard.tech>
2020-05-24 01:46:43 +0900

    Include GeoScopes in ChargingPoint and GasStation

--- app/models/charging_point.rb
+++ app/models/charging_point.rb
@@ -1,3 +1,5 @@
 class ChargingPoint < ApplicationRecord
   enum status: { '開設' => 1, '閉鎖' => 2, '不明' => 3, '休止' => 4 }
+
+  include GeoScopes
 end
--- app/models/gas_station.rb
+++ app/models/gas_station.rb
@@ -1,3 +1,5 @@
 class GasStation < ApplicationRecord
   enum status: { '開設' => 1, '閉鎖' => 2, '不明' => 3 }
+
+  include GeoScopes
 end
